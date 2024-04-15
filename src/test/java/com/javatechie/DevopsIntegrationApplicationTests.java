eb3aa9b9aa5c45bc6abd430512a6f28dcd27b8fb
Usaid-Bhirya <103103161+Usaid-Bhirya@users.noreply.github.com>
2024-04-15 20:20:29 +0500

    Test errors resolved

--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -1,7 +1,9 @@
+package com.javatechie;
 
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class DevopsIntegrationApplicationTests {
